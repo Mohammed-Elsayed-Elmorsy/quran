cc420772486a97356969c8c71aa59976c1529e5d
mohammed <sallealamohammed@gmial.com>
2025-01-06 01:59:47 +0200

    the new version came again6

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -10,13 +10,13 @@ const TopNav = () => {
   const state = localStorage.getItem('lang') ? JSON.parse(localStorage.getItem('lang')) : lang
   return (
     <div className=' upper-header bg-primary'>
-      <div className="container mx-auto h-[70px]  px-8 flex justify-between items-center">
+      <div className="container mx-auto h-[80px]  px-8 flex justify-between items-center">
         <Link to={'/programms'}>
-          <span className=' hover:bg-white hover:text-black transition-all  bg-secondary   block py-2 px-3 text-[20px] text-slate-100'>
+          <span className=' hover:bg-white hover:text-black transition-all  bg-secondary   block py-3 px-3 text-[20px] text-slate-100'>
             {state ? 'ابدا معنا الان' : 'Start with us now'}
           </span>
         </Link>
-        <div className=' flex gap-3 items-center text-[25px] upper-span'>
+        <div className=' flex gap-3 items-center text-[30px] upper-span'>
           <a className=' hover:bg-white hover:text-black p-[4px] transition-all rounded' href="https://wa.me/201212659525" target=' _blank'>
             <FaWhatsapp />
           </a>
